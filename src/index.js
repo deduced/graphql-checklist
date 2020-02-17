1117444b4a6e3a05ffa3a4b08ab1310f3e1871ad
Charlie Astrada <carlosastrada@gmail.com>
2020-02-17 09:27:15 -0300

    Fix bug with ApolloProvider import
    Named import so needed curly braces.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import ApolloClient from "apollo-boost";
-import ApolloProvider from "@apollo/react-hooks";
+import { ApolloProvider } from "@apollo/react-hooks";
 
 const client = new ApolloClient({
   uri: "https://react-todo-graphql-dev.herokuapp.com/v1/graphql"
