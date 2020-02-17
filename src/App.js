384be1fa8ca8263872887101e9bd5daa779f119e
Charlie Astrada <carlosastrada@gmail.com>
2020-02-17 12:09:06 -0300

    Add basic listing of todos
    Todos are queried via apollo client and graphql to the HASANA endpoint. A non-working form for adding todo (HTML placeholder) is in place. Code maps over the returned todos and lists them.
    
    * no styling so ugly!

--- src/App.js
+++ src/App.js
@@ -1,7 +1,42 @@
 import React from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+
+const GET_TODOS = gql`
+  query getAllTodos {
+    todos {
+      done
+      id
+      text
+    }
+  }
+`;
 
 function App() {
-  return <div>GraphQL Checklist App</div>;
+  const { data, loading, error } = useQuery(GET_TODOS);
+
+  if (loading) return <div>Loading todos...</div>;
+  if (error) return <div>Error fetching todos!</div>;
+
+  return (
+    <div>
+      <h1>GraphQL Checklist</h1>
+      {/* Todo Form */}
+      <form>
+        <input type="text" placeholder="Write your todo" />
+        <button type="submit">Create</button>
+      </form>
+      {/* Todo List */}
+      <div>
+        {data.todos.map(todo => (
+          <p key={todo.id}>
+            <span>{todo.text}</span>
+            <button>&times;</button>
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
