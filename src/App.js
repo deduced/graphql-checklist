80597660609c90de88667aa8146f8e08e59ab502
Charlie Astrada <carlosastrada@gmail.com>
2020-02-17 23:13:48 -0300

    Add logic for Deleting todos
    * Add handleDeleteTodo
    * Add handler to html button
    * use update() from apollo client for updating cache the proper way after deletion

--- src/App.js
+++ src/App.js
@@ -36,6 +36,18 @@ const ADD_TODO = gql`
   }
 `;
 
+const DELETE_TODO = gql`
+  mutation deleteTodo($id: uuid!) {
+    delete_todos(where: { id: { _eq: $id } }) {
+      returning {
+        done
+        text
+        id
+      }
+    }
+  }
+`;
+
 function App() {
   const { data, loading, error } = useQuery(GET_TODOS);
   const [toggleTodo] = useMutation(TOGGLE_TODO);
@@ -43,6 +55,8 @@ function App() {
     onCompleted: () => setTodoText("")
   });
   const [todoText, setTodoText] = useState("");
+
+  const [deleteTodo] = useMutation(DELETE_TODO);
 
   async function handleToggleTodo({ id, done }) {
     const result = await toggleTodo({ variables: { id: id, done: !done } });
@@ -53,10 +67,32 @@ function App() {
     event.preventDefault();
     const result = await addTodo({
       variables: { text: todoText },
+      //refetchQueries is not the way to refresh cache. Just did it for practice. Use cache update
       refetchQueries: [{ query: GET_TODOS }]
     });
     console.log("added todo", result.data.insert_todos.returning);
     // setTodoText("");
+  }
+
+  async function handleDeleteTodo({ id }) {
+    const isConfirmed = window.confirm(
+      "Are you sure you want to delete this todo?"
+    );
+
+    if (isConfirmed) {
+      const result = await deleteTodo({
+        variables: { id },
+        update: cache => {
+          const prevCache = cache.readQuery({ query: GET_TODOS });
+          const updatedTodos = prevCache.todos.filter(todo => todo.id !== id);
+          cache.writeQuery({ query: GET_TODOS, data: { todos: updatedTodos } });
+        }
+      });
+
+      console.log("deleted todo", result.data.delete_todos.returning);
+    } else {
+      return;
+    }
   }
 
   if (loading) return <div>Loading todos...</div>;
@@ -97,14 +133,18 @@ function App() {
               name={todo.id}
               onChange={() => handleToggleTodo(todo)}
               type="checkbox"
+              className="pointer pa2 f4"
             />
             <label htmlFor={todo.id}>
               <span className={`pointer pa1 f4 ${todo.done && "strike"}`}>
                 {todo.text}
               </span>
             </label>
-            <button className="bg-transparent bn f4">
-              <span className="red">ⅹ</span>
+            <button
+              className="bg-transparent bn f4"
+              onClick={() => handleDeleteTodo(todo)}
+            >
+              <span className="red pa1 pointer hover-bg-blue">ⅹ</span>
             </button>
           </p>
         ))}
